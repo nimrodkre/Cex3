d7cd1930eccb395dc7ab2e87b429cb20c765d798
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2020-05-19 08:35:55 +0300

    built new tree function

--- RBTree.c
+++ RBTree.c
@@ -1,4 +1,20 @@
 //
 // Created by User on 19/05/2020.
 //
+#include "RBTree.h"
+#include <stdlib.h>
+
+RBTree *newRBTree(CompareFunc compFunc, FreeFunc freeFunc)
+{
+    RBTree *tree = (RBTree *)calloc(1, sizeof(RBTree));
+    if (tree == NULL)
+    {
+        return NULL;
+    }
+    tree->freeFunc = freeFunc;
+    tree->compFunc = compFunc;
+    tree->root = NULL;
+    tree->size = 0;
+    return tree;
+}
 
